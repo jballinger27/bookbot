7e38c470e81aa3e58a1603473d293bac42f4f37c
Jeffrey Ballinger <jballinger27@pm.me>
2025-08-05 20:20:38 -0400

    more progress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from stats import number_of_words
+from stats import number_of_words, character_usage, sort_usage
 
 def get_book_text(rel_path):
     with open(rel_path) as f:
@@ -8,6 +8,14 @@ def get_book_text(rel_path):
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     #print(book_text)
-    print(f"{number_of_words(book_text)} words found in the document")
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Found {number_of_words(book_text)} total words")
+    print("--------- Character Count -------")
+    usage = character_usage(book_text)
+    sorted_usage = sort_usage(usage)
+    for pair in sorted_usage:
+        print(f"{pair["char"]}: {pair["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,20 @@
 def number_of_words(text):
     return len(text.split())
+
+def character_usage(text):
+    usage = {}
+    for char in text.lower():
+        if(char in usage):
+            usage[char] += 1
+        else :
+            usage[char] = 1
+    return usage
+
+def sort_usage(usage_dict):
+    sorted_usage = []
+    for character in usage_dict.keys():
+        if character.isalpha():
+            pair = {"char": character, "num": usage_dict[character]}
+            sorted_usage.append(pair)
+    sorted_usage.sort(reverse=True, key=lambda x: x["num"])
+    return sorted_usage
