d0ec7045f4be859281738b21c2d3fb3edcfde91a
Jeffrey Ballinger <jballinger@coderspastime.com>
2025-08-06 08:44:46 +0000

    finishing touches for now

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 
 from stats import number_of_words, character_usage, sort_usage
-
+import sys
 def get_book_text(rel_path):
     with open(rel_path) as f:
         return f.read()
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    #print(book_text)
+    book_text = ""
+    if len(sys.argv) > 1:
+        book_text = get_book_text(sys.argv[1])
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print(f"Found {number_of_words(book_text)} total words")
     print("--------- Character Count -------")
     usage = character_usage(book_text)
